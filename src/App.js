ccb4d46a8fcd1d9d9c608d1dbf7f52eb1d179fdd
Ekimerton <ekim0252@gmail.com>
2021-05-31 02:22:00 -0400

    ✨  Added search bar for class codes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { AutoComplete } from 'antd';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [autocompleteOptions, setAutoCompleteOptions] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [selectedClasses, setSelectedClasses] = useState([]);
+
+  useEffect(() => {
+    const loadAutocomplete = async () => {
+      const res = await axios.get(
+        'https://classio-api.herokuapp.com/course',
+        { params: { semester: "2021 Winter" } }
+      );
+      const course_codes = res.data.course_codes;
+      const course_options = [];
+      course_codes.map(course_code => {
+        course_options.push({ 'value': course_code })
+      })
+
+      setAutoCompleteOptions(course_options)
+    }
+    loadAutocomplete();
+  }, []);
+
+  const onSelectCourse = (courseCode) => {
+    const newSelectedClasses = [...selectedClasses]
+    newSelectedClasses.push(courseCode);
+    setSelectedClasses(newSelectedClasses);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <AutoComplete
+          value={searchValue}
+          options={autocompleteOptions}
+          style={{ width: 200 }}
+          onSelect={(courseCode) => { onSelectCourse(courseCode) }}
+          onSearch={() => { }}
+          onChange={setSearchValue}
+          placeholder="control mode"
+          filterOption={(inputValue, option) =>
+            option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+          }
+        />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {selectedClasses}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
